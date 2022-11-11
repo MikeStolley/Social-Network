74c98d7cfb50b1ccf646aba2011e9d90e757414f
Mike Stolley <stolleywilliam@yahoo.com>
2022-11-11 17:26:24 -0500

    finished work on controllers

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -9,16 +9,58 @@ const uController = {
     },
 
     getAllUsers(req, res) {
-        Users({})
+        Users.find({})
         .populate({path: 'thoughts'})
         .populate({path: 'friends'})
         .then(dbUsersData => res.json(dbUsersData))
         .catch(err => {
             res.status(500).json(err)
         })
+    },
+
+    getUsersById({params}, res) {
+        Users.findOne({_id: params.id })
+        .populate({path: 'thoughts'})
+        .populate({path: 'friends'})
+        .then(dbUsersData => {
+            if(!dbUsersData) {
+                res.status(404).json({message: 'None found' })
+                return;
+            }
+            res.json(dbUsersData)
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        })
+    },
+
+    updateUser({params, body}, res) {
+        Users.findOneAndUpdate({_id: params.id }, body, {new: true, runValidators: true})
+        .then(dbUsersData => {
+            if(!dbUsersData) {
+                res.status(404).json({message: 'None found' })
+                return;
+            }
+            res.json(dbUsersData)
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        })
+    },
+
+    deleteUser({params}, res) {
+        Users.findOneAndDelete({_id: params.id })
+        .then(dbUsersData => {
+            if(!dbUsersData) {
+                res.status(404).json({message: 'None found' })
+                return;
+            }
+            res.json(dbUsersData)
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        })
     }
+};
 
-
-
-
-}
+module.exports = uController;
