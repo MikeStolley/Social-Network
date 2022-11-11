e3e93b086801e4b649c8f2b5e0891d695cb51d0e
Mike Stolley <stolleywilliam@yahoo.com>
2022-11-11 17:56:11 -0500

    fixed start issue where reactions schema was out of order

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,33 +1,6 @@
 const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
 
-const ThoughtsSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            minlength: 1,
-            maxlength: 200
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        reactions: [ReactionsSchema]
-    },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true
-        },
-        id: false
-    }
-);
-
 const ReactionsSchema = new Schema(
     {
         reactionId: {
@@ -52,6 +25,33 @@ const ReactionsSchema = new Schema(
         toJSON: {
             getters: true
         }
+    }
+);
+
+const ThoughtsSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 200
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [ReactionsSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
 );
 
